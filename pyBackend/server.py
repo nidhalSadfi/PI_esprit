b634e56d8098456c463cf494d7bae3a639a5ab89
nidhal.Sadfi <nidhal.sadfi@gmail.com>
2024-07-05 00:39:33 +0200

    updates on the server files

--- pyBackend/server.py
+++ pyBackend/server.py
@@ -10,22 +10,27 @@ collection = db.gas
 
 @app.route('/data', methods=['POST'])
 def receive_data():
-    temperature = request.form.get('temperature')
-    humidity = request.form.get('humidity')
-    pressure = request.form.get('pressure')
-
-    if temperature and humidity and pressure:
+    data = {}
+    if 'temperature' in request.form and 'humidity' in request.form and 'pressure' in request.form:
         data = {
-            'temperature': float(temperature),
-            'humidity': float(humidity),
-            'pressure': float(pressure),
+            'temperature': float(request.form.get('temperature')),
+            'humidity': float(request.form.get('humidity')),
+            'pressure': float(request.form.get('pressure')),
             'timestamp': datetime.datetime.now()
         }
-        result = collection.insert_one(data)
-        data['_id'] = str(result.inserted_id)  # Convert ObjectId to string
-        return jsonify({"status": "success", "data": data}), 200
+    elif 'eco2' in request.form and 'etvoc' in request.form and 'ozone' in request.form:
+        data = {
+            'eco2': float(request.form.get('eco2')),
+            'etvoc': float(request.form.get('etvoc')),
+            'ozone': float(request.form.get('ozone')),
+            'timestamp': datetime.datetime.now()
+        }
     else:
         return jsonify({"status": "error", "message": "Invalid data"}), 400
+    
+    result = collection.insert_one(data)
+    data['_id'] = str(result.inserted_id)  # Convert ObjectId to string
+    return jsonify({"status": "success", "data": data}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
